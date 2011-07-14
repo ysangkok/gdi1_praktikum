cd3cd9b136ffe3817e37eeb498e685982b04535a
yanai <yanai@a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8>
2011-07-14 11:41:23 +0000

    not yet executable
    git-svn-id: file:///home/janus/ntfs-backup/gdi1_projekt@21 a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8

---
+++ schiffe_versenken/src/gruppe16/highScore.java
@@ -0,0 +1,56 @@
+package gruppe16;
+
+
+import java.util.*;
+
+
+
+public class highScore {
+
+	private class highScoreEntry {
+
+		// defines the name of player
+		private String name;
+		// defines the score reached
+		private double score;
+		// the time the game started
+		public long time;
+		// defines the date the game started
+		public String date;
+
+		/**
+		 * 
+		 * 
+		 * Constructor
+		 */
+
+		public highScoreEntry(String playerName, double scoreEntry) {
+
+			this.name = playerName;
+			this.score = scoreEntry;
+
+		}
+		
+		
+		
+		public final String getName(){
+			return name;
+			
+		}
+		
+		
+		public void setHighScore(final int score){
+			
+			
+			
+		}
+
+		
+		
+		public sortList(){
+			
+			
+		}
+	}
+
+}
