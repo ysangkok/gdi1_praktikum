58ba467ff2007aff71ede87295b1e2576cdc6986
oh05keny <oh05keny@a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8>
2011-07-14 17:21:37 +0000

    update guischiffe
    git-svn-id: file:///home/janus/ntfs-backup/gdi1_projekt@26 a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8

--- schiffe_versenken/src/gruppe16/GUISchiffe.java
+++ schiffe_versenken/src/gruppe16/GUISchiffe.java
@@ -6,22 +6,32 @@ import gruppe16.gui.BoardPanel;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class GUISchiffe /*implements KeyListener*/ {
-	public static void main(String[] args) {
+	JFrame frame;
+	
+	public void GameOver() {
+		JOptionPane.showMessageDialog(frame,
+			    "Eggs are not supposed to be green.", 
+			    "Message", 0);
+		frame.dispose();
+	}
+	
+	GUISchiffe() {
 		Engine engine = new Engine();
 		System.out.println(engine.getLevelStringForPlayer(0));
 		
 		AI ai = new GoodAI(engine);
 		
-		JFrame frame = new JFrame("GUISchiffe");
+		frame = new JFrame("GUISchiffe");
 		JPanel panel = new JPanel();
 		BoardPanel panel1 = null;
 		BoardPanel panel2 = null;
-		panel1 = new BoardPanel(frame, engine, 0, ai);
+		panel1 = new BoardPanel(this, engine, 0, ai);
 		panel.add(panel1);
-		panel2 = new BoardPanel(frame, engine, 1, ai);
+		panel2 = new BoardPanel(this, engine, 1, ai);
 		panel.add(panel2);
 		
 		panel1.setOtherBoard(panel2);
@@ -32,5 +42,9 @@ public class GUISchiffe /*implements KeyListener*/ {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new GUISchiffe();
 	}
 }
