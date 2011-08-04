0b2dd69a66d834e61030c37bfbec61735e36a863
yanai <yanai@a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8>
2011-08-04 10:33:12 +0000

    HighScore update
    git-svn-id: file:///home/janus/ntfs-backup/gdi1_projekt@65 a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8

--- schiffe_versenken/src/gruppe16/Score.java
+++ schiffe_versenken/src/gruppe16/Score.java
@@ -1,38 +1,44 @@
 package gruppe16;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Score  implements Serializable {
     private int score;
-    private String naam;
-    private String neededTime;
-    private String date;
+    private String name;
+    private int neededTime;
+    private Date date;
 
     public int getScore() {
         return score;
     }
 
-    public String getNaam() {
-        return naam;
+    public String getName() {
+        return name;
     }
 
     
     
-    public String getNeededTime(){
+    public int getNeededTime(){
     	
     	return neededTime;
     }
     
     
-    public String getDate(){
+    public Date getDate(){
     	
       return date;
     }
 
+    public String toString () {
+    	return getName() + "\t\t" + getScore() +
+		"\t" + getNeededTime() + "\t" + getDate();
+    }
 
-
-    public Score(String naam, int score, String neededTime, String date) {
+    public Score(String name, int score, int neededTime, Date date) {
         this.score = score;
-        this.naam = naam;
+        this.name = name;
         this.neededTime= neededTime;
         this.date= date;   
     }
--- schiffe_versenken/src/gruppe16/ScoreComparator.java
+++ schiffe_versenken/src/gruppe16/ScoreComparator.java
@@ -1,12 +1,29 @@
 package gruppe16;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Map;
 
 public class ScoreComparator implements Comparator<Score> {
+		private String criteria;
+
+		public ScoreComparator(String sortingcritiria) {
+			this.criteria = sortingcritiria;
+		}
+	
         public int compare(Score score1, Score score2) {
 
-            int sc1 = score1.getScore();
-            int sc2 = score2.getScore();
-
+            if (criteria.equals("score")) {
+            	return ((Integer) score1.getScore()).compareTo(score2.getScore());
+            } else if (criteria.equals("name")) {
+            	return score1.getName().compareTo(score2.getName());
+            } else if (criteria.equals("neededTime")) {
+            	return ((Integer) score1.getNeededTime()).compareTo(score2.getNeededTime());
+            } else if (criteria.equals("date")) {
+            	return score1.getDate().compareTo(score2.getDate());
+            } else {
+            	throw new RuntimeException("Invalid criteria: " + criteria);
+            }
+            /*
             if (sc1 > sc2){
                 return -1;
             }else if (sc1 < sc2){
@@ -14,5 +31,6 @@ public class ScoreComparator implements Comparator<Score> {
             }else{
                 return 0;
             }
+            */
         }
 }
