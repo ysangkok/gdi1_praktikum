d0559289d45a592fc71c9ca2e73f30a734e1de5b
dimitrios <dimitrios@a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8>
2011-08-15 11:52:50 +0000

    greek language
    git-svn-id: file:///home/janus/ntfs-backup/gdi1_projekt@137 a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8

---
+++ gwittest/war/template/Battleship.el
@@ -0,0 +1,127 @@
+multi=ένα {0} δύο {1} τρία {2} παράμετροι
+noSuchKeyException=Δεν βρέθυκε μέσο για {0}!
+hitCounter=You: {0} Χτυπήματα : Computer: {1} Χτυπήματα 
+
+guiFrame=Ναυμαχία
+
+//LanguageMenu
+languageMenu.label=Γλώσσα
+languageMenu.toolTipText= Επέλεξε γλώσσα
+German.label=Γερμανικά
+German.toolTipText=Γερμανικά
+English.label=Αγγλικά
+English.toolTipText=Αγγλικά
+Russian.label=Ρώσικα
+Russian.toolTipText=Ρώσικα
+Greek.label=Ελληνικά
+Greek.toolTipText=Ρώσικα
+
+//file
+fileMenu.label=Αρχείο
+fileMenu.toolTipText=Αρχείο
+
+//Options
+Options.label=Επιλογές
+Options.toolTipText=Επιλογές
+
+//New
+New.label=Νέο...
+New.toolTipText=Νέο
+
+//About
+About.label=Σχετικά για εμάς
+About.toolTipText=Σχετικά για εμάς
+
+//QuickNew
+QuickNew.label=Φόρτιση επιπέδου
+QuickNew.toolTipText=Load built-in level
+QuickNew.mnemonic=n
+
+//PlaceShips
+PlaceShips.label=Τοποθέτηση πλοίων
+PlaceShips.toolTipText=Tοποθέτηση πλοίων
+PlaceShips.mnemonic=p
+
+//DefaultNewGame
+DefaultNewGame.label=Τυχαία τοποθέτηση πλοίων
+DefaultNewGame.toolTipText=Tυχαία
+DefaultNewGame.mnemonic=g
+
+//SaveGame
+saveGame.label=Απθήκευση παιχνιδιού
+saveGame.toolTipText=Απθήκευση παιχνιδιού
+saveGame.mnemonic=control S
+
+//loadItem
+loadGame.label=Φόρτιση παιχνιδιού
+loadGame.toolTipText=Φόρτιση παιχνιδιού
+loadGame.mnemonic=control L
+
+quit.label=Κλείσιμο
+quit.toolTipText=Κλεισίμο
+
+//Game over
+gameOverDraw=Ισοπαλία.
+gameOverWinner=Παίχτης {0} κέρδισε!
+gameOverWindowTitle=Τέλος παιχνιδιού
+
+// 0 ist hier gameOverWinner/Draw
+gameOverReasonLabel=Τέλος παιχνιδιού. {0} λόγου: {1}
+gameOverQuit=Τέλος
+gameOverStartOver=Νέο ξεκίνημα
+
+//Constructor
+constructorShooterSelect=Επέλεξε σκοπευτή
+constructorJustAttack=Σφαίρες του πλοίου απενεργοποιήτες, κανε επίθεση στο πεδίο στη δεξιά πλευρά.
+
+//Error
+userErrorWindowTitle=Σφάλμα
+
+//About
+aboutContent=Gruppe 16: Dimitrios, Janus, Alymbek, Yanai
+aboutWindowTitle=Σχετικά για εμάς
+
+//Shots
+bombSelectShooter=Selected ({0},{1}): {2} Σφαίρες απομενουν
+bombRemainingShots=Remaining shots: {0}
+
+//Buttons
+OK=OK
+Cancel=Cancel
+
+//SC
+SCWindowTitle=Επέλεξε τρόπο παιχνιδιού
+SCSpeerCheckBox=Μπαράζ βολών ενεργοποιημένο 
+SCAmmoCheckBox=Πυρομαχικά ενεργοποιημένα
+SCSpeerLabel=Χρόνος δευτερολέπτων ανα γύρο:
+SCAmmoLabel=Πυρομαχικά πλοίου σε κάθε πεδίο:
+SCAILabel=AI:
+SCAIUncapable=<html>{0} Δεν υποστηρίζει πυρομαχικά.χρησιμοποίησε άλλο AI.</html>
+SCMoreShots=Επιτρέπει και επιπλέων βολές σε περίπτωση χτυπήματος
+SCHeightLabel=Ύψος επιφάνειας:
+SCWidthLabel=Πλάτος επιφάνειας:
+
+//Place ships
+POSDN_Ship={0}-πλοίο:
+POSDPlace=Σημείο 
+POSDWindowTitle=Τοποθέτησε τα πλοία σου
+
+CRHorizontal=Οριζόντια
+CRVertical=Κάθετα
+CRWindowTitle=Πάτησε κάποιο σημείο
+CRLabel=<html>Επέλεξε προσανατολισμό και πάτησε<br>πάνω/αριστερά τέλος του πλοίου στην επιφανεια.</html>
+
+//Messages
+RNoMoreAmmo=<html>Το επιλεγμένο σημείο έμεινε απο πυρομαχικά. Επέλεξε άλλο σημείο.</html>
+RNoShooterDesignated=<html>Δεν εχεις ορίσει σκοπευτή.Πάτησε κάποιο σημείο στην επιφάνεια <αριστερά> για να το πράξεις.</html>
+RNotShootable=<html>Δεν μπορείς να χτυπήσεις αυτο το σημείο.Δεν επιτρέπετε το χτύπημα σε ιδι χτυπημένο σημείο.</html> 
+RNotYourTurn=<html>Περίμενε να έρθει ι σειρά σου.</html>
+
+//sound
+enableSounds.label=Ήχους ενεργοποιημένος
+enableSounds.toolTipText=Ήχους ενεργοποιημένος
+
+//skins 
+skinMenu.label=Επιφάνειες
+skinMenu.toolTipText=Επιφάνειες
+skinMenuChoose.label=Επέλεξε {0}
