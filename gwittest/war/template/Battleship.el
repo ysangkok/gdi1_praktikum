15dc59e1bce632a6b803423b49394082725bb75f
oh05keny <oh05keny@a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8>
2011-08-16 20:30:48 +0000

    try fix translations
    git-svn-id: file:///home/janus/ntfs-backup/gdi1_projekt@153 a36dc4ef-63f6-4d2a-8235-54e5dfe6f4d8

--- gwittest/war/template/Battleship.el
+++ gwittest/war/template/Battleship.el
@@ -1,91 +1,91 @@
-multi=ένα {0} δύο {1} τρία {2} παράμετροι
-noSuchKeyException=Δεν βρέθυκε μέσο για {0}!
-hitCounter=You: {0} Χτυπήματα : Computer: {1} Χτυπήματα 
+multi=\u03ad\u03bd\u03b1 {0} \u03b4\u03cd\u03bf {1} \u03c4\u03c1\u03af\u03b1 {2} \u03c0\u03b1\u03c1\u03ac\u03bc\u03b5\u03c4\u03c1\u03bf\u03b9
+noSuchKeyException=\u0394\u03b5\u03bd \u03b2\u03c1\u03ad\u03b8\u03c5\u03ba\u03b5 \u03bc\u03ad\u03c3\u03bf \u03b3\u03b9\u03b1 {0}!
+hitCounter=You: {0} \u03a7\u03c4\u03c5\u03c0\u03ae\u03bc\u03b1\u03c4\u03b1 : Computer: {1} \u03a7\u03c4\u03c5\u03c0\u03ae\u03bc\u03b1\u03c4\u03b1 
 
-guiFrame=Ναυμαχία
+guiFrame=\u039d\u03b1\u03c5\u03bc\u03b1\u03c7\u03af\u03b1
 
 //LanguageMenu
-languageMenu.label=Γλώσσα
-languageMenu.toolTipText= Επέλεξε γλώσσα
-German.label=Γερμανικά
-German.toolTipText=Γερμανικά
-English.label=Αγγλικά
-English.toolTipText=Αγγλικά
-Russian.label=Ρώσικα
-Russian.toolTipText=Ρώσικα
-Greek.label=Ελληνικά
-Greek.toolTipText=Ρώσικα
+languageMenu.label=\u0393\u03bb\u03ce\u03c3\u03c3\u03b1
+languageMenu.toolTipText= \u0395\u03c0\u03ad\u03bb\u03b5\u03be\u03b5 \u03b3\u03bb\u03ce\u03c3\u03c3\u03b1
+German.label=\u0393\u03b5\u03c1\u03bc\u03b1\u03bd\u03b9\u03ba\u03ac
+German.toolTipText=\u0393\u03b5\u03c1\u03bc\u03b1\u03bd\u03b9\u03ba\u03ac
+English.label=\u0391\u03b3\u03b3\u03bb\u03b9\u03ba\u03ac
+English.toolTipText=\u0391\u03b3\u03b3\u03bb\u03b9\u03ba\u03ac
+Russian.label=\u03a1\u03ce\u03c3\u03b9\u03ba\u03b1
+Russian.toolTipText=\u03a1\u03ce\u03c3\u03b9\u03ba\u03b1
+Greek.label=\u0395\u03bb\u03bb\u03b7\u03bd\u03b9\u03ba\u03ac
+Greek.toolTipText=\u03a1\u03ce\u03c3\u03b9\u03ba\u03b1
 
 //file
-fileMenu.label=Αρχείο
-fileMenu.toolTipText=Αρχείο
+fileMenu.label=\u0391\u03c1\u03c7\u03b5\u03af\u03bf
+fileMenu.toolTipText=\u0391\u03c1\u03c7\u03b5\u03af\u03bf
 
 //Options
-Options.label=Επιλογές
-Options.toolTipText=Επιλογές
+Options.label=\u0395\u03c0\u03b9\u03bb\u03bf\u03b3\u03ad\u03c2
+Options.toolTipText=\u0395\u03c0\u03b9\u03bb\u03bf\u03b3\u03ad\u03c2
 
 //New
-New.label=Νέο...
-New.toolTipText=Νέο
+New.label=\u039d\u03ad\u03bf...
+New.toolTipText=\u039d\u03ad\u03bf
 
 //About
-About.label=Σχετικά για εμάς
-About.toolTipText=Σχετικά για εμάς
+About.label=\u03a3\u03c7\u03b5\u03c4\u03b9\u03ba\u03ac \u03b3\u03b9\u03b1 \u03b5\u03bc\u03ac\u03c2
+About.toolTipText=\u03a3\u03c7\u03b5\u03c4\u03b9\u03ba\u03ac \u03b3\u03b9\u03b1 \u03b5\u03bc\u03ac\u03c2
 
 //Highscore
-Highscore.label=βαθμολογικός πίνακας
+Highscore.label=\u03b2\u03b1\u03b8\u03bc\u03bf\u03bb\u03bf\u03b3\u03b9\u03ba\u03cc\u03c2 \u03c0\u03af\u03bd\u03b1\u03ba\u03b1\u03c2
 
 //QuickNew
-QuickNew.label=Φόρτιση επιπέδου
+QuickNew.label=\u03a6\u03cc\u03c1\u03c4\u03b9\u03c3\u03b7 \u03b5\u03c0\u03b9\u03c0\u03ad\u03b4\u03bf\u03c5
 QuickNew.toolTipText=Load built-in level
 QuickNew.mnemonic=n
 
 //PlaceShips
-PlaceShips.label=Τοποθέτηση πλοίων
-PlaceShips.toolTipText=Tοποθέτηση πλοίων
+PlaceShips.label=\u03a4\u03bf\u03c0\u03bf\u03b8\u03ad\u03c4\u03b7\u03c3\u03b7 \u03c0\u03bb\u03bf\u03af\u03c9\u03bd
+PlaceShips.toolTipText=T\u03bf\u03c0\u03bf\u03b8\u03ad\u03c4\u03b7\u03c3\u03b7 \u03c0\u03bb\u03bf\u03af\u03c9\u03bd
 PlaceShips.mnemonic=p
 
 //DefaultNewGame
-DefaultNewGame.label=Τυχαία τοποθέτηση πλοίων
-DefaultNewGame.toolTipText=Tυχαία
+DefaultNewGame.label=\u03a4\u03c5\u03c7\u03b1\u03af\u03b1 \u03c4\u03bf\u03c0\u03bf\u03b8\u03ad\u03c4\u03b7\u03c3\u03b7 \u03c0\u03bb\u03bf\u03af\u03c9\u03bd
+DefaultNewGame.toolTipText=T\u03c5\u03c7\u03b1\u03af\u03b1
 DefaultNewGame.mnemonic=g
 
 //SaveGame
-saveGame.label=Απθήκευση παιχνιδιού
-saveGame.toolTipText=Απθήκευση παιχνιδιού
+saveGame.label=\u0391\u03c0\u03b8\u03ae\u03ba\u03b5\u03c5\u03c3\u03b7 \u03c0\u03b1\u03b9\u03c7\u03bd\u03b9\u03b4\u03b9\u03bf\u03cd
+saveGame.toolTipText=\u0391\u03c0\u03b8\u03ae\u03ba\u03b5\u03c5\u03c3\u03b7 \u03c0\u03b1\u03b9\u03c7\u03bd\u03b9\u03b4\u03b9\u03bf\u03cd
 saveGame.mnemonic=control S
 
 //loadItem
-loadGame.label=Φόρτιση παιχνιδιού
-loadGame.toolTipText=Φόρτιση παιχνιδιού
+loadGame.label=\u03a6\u03cc\u03c1\u03c4\u03b9\u03c3\u03b7 \u03c0\u03b1\u03b9\u03c7\u03bd\u03b9\u03b4\u03b9\u03bf\u03cd
+loadGame.toolTipText=\u03a6\u03cc\u03c1\u03c4\u03b9\u03c3\u03b7 \u03c0\u03b1\u03b9\u03c7\u03bd\u03b9\u03b4\u03b9\u03bf\u03cd
 loadGame.mnemonic=control L
 
-quit.label=Κλείσιμο
-quit.toolTipText=Κλεισίμο
+quit.label=\u039a\u03bb\u03b5\u03af\u03c3\u03b9\u03bc\u03bf
+quit.toolTipText=\u039a\u03bb\u03b5\u03b9\u03c3\u03af\u03bc\u03bf
 
 //Game over
-gameOverDraw=Ισοπαλία.
-gameOverWinner=Παίχτης {0} κέρδισε!
-gameOverWindowTitle=Τέλος παιχνιδιού
+gameOverDraw=\u0399\u03c3\u03bf\u03c0\u03b1\u03bb\u03af\u03b1.
+gameOverWinner=\u03a0\u03b1\u03af\u03c7\u03c4\u03b7\u03c2 {0} \u03ba\u03ad\u03c1\u03b4\u03b9\u03c3\u03b5!
+gameOverWindowTitle=\u03a4\u03ad\u03bb\u03bf\u03c2 \u03c0\u03b1\u03b9\u03c7\u03bd\u03b9\u03b4\u03b9\u03bf\u03cd
 
 // 0 ist hier gameOverWinner/Draw
-gameOverReasonLabel=Τέλος παιχνιδιού. {0} λόγου: {1}
-gameOverQuit=Τέλος
-gameOverStartOver=Νέο ξεκίνημα
+gameOverReasonLabel=\u03a4\u03ad\u03bb\u03bf\u03c2 \u03c0\u03b1\u03b9\u03c7\u03bd\u03b9\u03b4\u03b9\u03bf\u03cd. {0} \u03bb\u03cc\u03b3\u03bf\u03c5: {1}
+gameOverQuit=\u03a4\u03ad\u03bb\u03bf\u03c2
+gameOverStartOver=\u039d\u03ad\u03bf \u03be\u03b5\u03ba\u03af\u03bd\u03b7\u03bc\u03b1
 
 //Constructor
-constructorShooterSelect=Επέλεξε σκοπευτή
-constructorJustAttack=Σφαίρες του πλοίου απενεργοποιήτες, κανε επίθεση στο πεδίο στη δεξιά πλευρά.
+constructorShooterSelect=\u0395\u03c0\u03ad\u03bb\u03b5\u03be\u03b5 \u03c3\u03ba\u03bf\u03c0\u03b5\u03c5\u03c4\u03ae
+constructorJustAttack=\u03a3\u03c6\u03b1\u03af\u03c1\u03b5\u03c2 \u03c4\u03bf\u03c5 \u03c0\u03bb\u03bf\u03af\u03bf\u03c5 \u03b1\u03c0\u03b5\u03bd\u03b5\u03c1\u03b3\u03bf\u03c0\u03bf\u03b9\u03ae\u03c4\u03b5\u03c2, \u03ba\u03b1\u03bd\u03b5 \u03b5\u03c0\u03af\u03b8\u03b5\u03c3\u03b7 \u03c3\u03c4\u03bf \u03c0\u03b5\u03b4\u03af\u03bf \u03c3\u03c4\u03b7 \u03b4\u03b5\u03be\u03b9\u03ac \u03c0\u03bb\u03b5\u03c5\u03c1\u03ac.
 
 //Error
-userErrorWindowTitle=Σφάλμα
+userErrorWindowTitle=\u03a3\u03c6\u03ac\u03bb\u03bc\u03b1
 
 //About
 aboutContent=Gruppe 16: Dimitrios, Janus, Alymbek, Yanai
-aboutWindowTitle=Σχετικά για εμάς
+aboutWindowTitle=\u03a3\u03c7\u03b5\u03c4\u03b9\u03ba\u03ac \u03b3\u03b9\u03b1 \u03b5\u03bc\u03ac\u03c2
 
 //Shots
-bombSelectShooter=Selected ({0},{1}): {2} Σφαίρες απομενουν
+bombSelectShooter=Selected ({0},{1}): {2} \u03a3\u03c6\u03b1\u03af\u03c1\u03b5\u03c2 \u03b1\u03c0\u03bf\u03bc\u03b5\u03bd\u03bf\u03c5\u03bd
 bombRemainingShots=Remaining shots: {0}
 
 //Buttons
@@ -93,46 +93,48 @@ OK=OK
 Cancel=Cancel
 
 //SC
-SCRange=εμβέλεια πλοίου ενεργοποιημένο
-SCShipLength=μήκος πλοίου
-SCShipCount=αριθμός πλοίων
-SCShipTypes=τίπος πλοίου
-SCShipCustomizeShips=προσαρμογή πλοίων
-SCDiffrentShipLengthCount=αριθμός πλοίων διάφορων μήκον 
-SCMustBePositiveError=λάθος πρέπει να είναι θετικό 
-SCEachSipTypeOnlyOnce=κάθε τίπος πλοίου μοναδικός
-SCWindowTitle=Επέλεξε τρόπο παιχνιδιού
-SCSpeerCheckBox=Μπαράζ βολών ενεργοποιημένο 
-SCAmmoCheckBox=Πυρομαχικά ενεργοποιημένα
-SCSpeerLabel=Χρόνος δευτερολέπτων ανα γύρο:
-SCAmmoLabel=Πυρομαχικά πλοίου σε κάθε πεδίο:
+SCRange=\u03b5\u03bc\u03b2\u03ad\u03bb\u03b5\u03b9\u03b1 \u03c0\u03bb\u03bf\u03af\u03bf\u03c5 \u03b5\u03bd\u03b5\u03c1\u03b3\u03bf\u03c0\u03bf\u03b9\u03b7\u03bc\u03ad\u03bd\u03bf
+SCShipLength=\u03bc\u03ae\u03ba\u03bf\u03c2 \u03c0\u03bb\u03bf\u03af\u03bf\u03c5
+SCShipCount=\u03b1\u03c1\u03b9\u03b8\u03bc\u03cc\u03c2 \u03c0\u03bb\u03bf\u03af\u03c9\u03bd
+SCShipTypes=\u03c4\u03af\u03c0\u03bf\u03c2 \u03c0\u03bb\u03bf\u03af\u03bf\u03c5
+SCShipCustomizeShips=\u03c0\u03c1\u03bf\u03c3\u03b1\u03c1\u03bc\u03bf\u03b3\u03ae \u03c0\u03bb\u03bf\u03af\u03c9\u03bd
+SCDiffrentShipLengthCount=\u03b1\u03c1\u03b9\u03b8\u03bc\u03cc\u03c2 \u03c0\u03bb\u03bf\u03af\u03c9\u03bd \u03b4\u03b9\u03ac\u03c6\u03bf\u03c1\u03c9\u03bd \u03bc\u03ae\u03ba\u03bf\u03bd 
+SCMustBePositiveError=\u03bb\u03ac\u03b8\u03bf\u03c2 \u03c0\u03c1\u03ad\u03c0\u03b5\u03b9 \u03bd\u03b1 \u03b5\u03af\u03bd\u03b1\u03b9 \u03b8\u03b5\u03c4\u03b9\u03ba\u03cc 
+SCEachSipTypeOnlyOnce=\u03ba\u03ac\u03b8\u03b5 \u03c4\u03af\u03c0\u03bf\u03c2 \u03c0\u03bb\u03bf\u03af\u03bf\u03c5 \u03bc\u03bf\u03bd\u03b1\u03b4\u03b9\u03ba\u03cc\u03c2
+SCWindowTitle=\u0395\u03c0\u03ad\u03bb\u03b5\u03be\u03b5 \u03c4\u03c1\u03cc\u03c0\u03bf \u03c0\u03b1\u03b9\u03c7\u03bd\u03b9\u03b4\u03b9\u03bf\u03cd
+SCSpeerCheckBox=\u039c\u03c0\u03b1\u03c1\u03ac\u03b6 \u03b2\u03bf\u03bb\u03ce\u03bd \u03b5\u03bd\u03b5\u03c1\u03b3\u03bf\u03c0\u03bf\u03b9\u03b7\u03bc\u03ad\u03bd\u03bf 
+SCAmmoCheckBox=\u03a0\u03c5\u03c1\u03bf\u03bc\u03b1\u03c7\u03b9\u03ba\u03ac \u03b5\u03bd\u03b5\u03c1\u03b3\u03bf\u03c0\u03bf\u03b9\u03b7\u03bc\u03ad\u03bd\u03b1
+SCSpeerLabel=\u03a7\u03c1\u03cc\u03bd\u03bf\u03c2 \u03b4\u03b5\u03c5\u03c4\u03b5\u03c1\u03bf\u03bb\u03ad\u03c0\u03c4\u03c9\u03bd \u03b1\u03bd\u03b1 \u03b3\u03cd\u03c1\u03bf:
+SCAmmoLabel=\u03a0\u03c5\u03c1\u03bf\u03bc\u03b1\u03c7\u03b9\u03ba\u03ac \u03c0\u03bb\u03bf\u03af\u03bf\u03c5 \u03c3\u03b5 \u03ba\u03ac\u03b8\u03b5 \u03c0\u03b5\u03b4\u03af\u03bf:
 SCAILabel=AI:
-SCAIUncapable=<html>{0} Δεν υποστηρίζει πυρομαχικά.χρησιμοποίησε άλλο AI.</html>
-SCMoreShots=Επιτρέπει και επιπλέων βολές σε περίπτωση χτυπήματος
-SCHeightLabel=Ύψος επιφάνειας:
-SCWidthLabel=Πλάτος επιφάνειας:
+SCAIUncapable=<html>{0} \u0394\u03b5\u03bd \u03c5\u03c0\u03bf\u03c3\u03c4\u03b7\u03c1\u03af\u03b6\u03b5\u03b9 \u03c0\u03c5\u03c1\u03bf\u03bc\u03b1\u03c7\u03b9\u03ba\u03ac.\u03c7\u03c1\u03b7\u03c3\u03b9\u03bc\u03bf\u03c0\u03bf\u03af\u03b7\u03c3\u03b5 \u03ac\u03bb\u03bb\u03bf AI.</html>
+SCMoreShots=\u0395\u03c0\u03b9\u03c4\u03c1\u03ad\u03c0\u03b5\u03b9 \u03ba\u03b1\u03b9 \u03b5\u03c0\u03b9\u03c0\u03bb\u03ad\u03c9\u03bd \u03b2\u03bf\u03bb\u03ad\u03c2 \u03c3\u03b5 \u03c0\u03b5\u03c1\u03af\u03c0\u03c4\u03c9\u03c3\u03b7 \u03c7\u03c4\u03c5\u03c0\u03ae\u03bc\u03b1\u03c4\u03bf\u03c2
+SCHeightLabel=\u038e\u03c8\u03bf\u03c2 \u03b5\u03c0\u03b9\u03c6\u03ac\u03bd\u03b5\u03b9\u03b1\u03c2:
+SCWidthLabel=\u03a0\u03bb\u03ac\u03c4\u03bf\u03c2 \u03b5\u03c0\u03b9\u03c6\u03ac\u03bd\u03b5\u03b9\u03b1\u03c2:
 
 //Place ships
-POSDN_Ship={0}-πλοίο:
-POSDPlace=Σημείο 
-POSDWindowTitle=Τοποθέτησε τα πλοία σου
+POSDN_Ship={0}-\u03c0\u03bb\u03bf\u03af\u03bf:
+POSDPlace=\u03a3\u03b7\u03bc\u03b5\u03af\u03bf 
+POSDWindowTitle=\u03a4\u03bf\u03c0\u03bf\u03b8\u03ad\u03c4\u03b7\u03c3\u03b5 \u03c4\u03b1 \u03c0\u03bb\u03bf\u03af\u03b1 \u03c3\u03bf\u03c5
 
-CRHorizontal=Οριζόντια
-CRVertical=Κάθετα
-CRWindowTitle=Πάτησε κάποιο σημείο
-CRLabel=<html>Επέλεξε προσανατολισμό και πάτησε<br>πάνω/αριστερά τέλος του πλοίου στην επιφανεια.</html>
+CRHorizontal=\u039f\u03c1\u03b9\u03b6\u03cc\u03bd\u03c4\u03b9\u03b1
+CRVertical=\u039a\u03ac\u03b8\u03b5\u03c4\u03b1
+CRWindowTitle=\u03a0\u03ac\u03c4\u03b7\u03c3\u03b5 \u03ba\u03ac\u03c0\u03bf\u03b9\u03bf \u03c3\u03b7\u03bc\u03b5\u03af\u03bf
+CRLabel=<html>\u0395\u03c0\u03ad\u03bb\u03b5\u03be\u03b5 \u03c0\u03c1\u03bf\u03c3\u03b1\u03bd\u03b1\u03c4\u03bf\u03bb\u03b9\u03c3\u03bc\u03cc \u03ba\u03b1\u03b9 \u03c0\u03ac\u03c4\u03b7\u03c3\u03b5<br>\u03c0\u03ac\u03bd\u03c9/\u03b1\u03c1\u03b9\u03c3\u03c4\u03b5\u03c1\u03ac \u03c4\u03ad\u03bb\u03bf\u03c2 \u03c4\u03bf\u03c5 \u03c0\u03bb\u03bf\u03af\u03bf\u03c5 \u03c3\u03c4\u03b7\u03bd \u03b5\u03c0\u03b9\u03c6\u03b1\u03bd\u03b5\u03b9\u03b1.</html>
 
 //Messages
-RNoMoreAmmo=<html>Το επιλεγμένο σημείο έμεινε απο πυρομαχικά. Επέλεξε άλλο σημείο.</html>
-RNoShooterDesignated=<html>Δεν εχεις ορίσει σκοπευτή.Πάτησε κάποιο σημείο στην επιφάνεια <αριστερά> για να το πράξεις.</html>
-RNotShootable=<html>Δεν μπορείς να χτυπήσεις αυτο το σημείο.Δεν επιτρέπετε το χτύπημα σε ιδι χτυπημένο σημείο.</html> 
-RNotYourTurn=<html>Περίμενε να έρθει ι σειρά σου.</html>
+RNoMoreAmmo=<html>\u03a4\u03bf \u03b5\u03c0\u03b9\u03bb\u03b5\u03b3\u03bc\u03ad\u03bd\u03bf \u03c3\u03b7\u03bc\u03b5\u03af\u03bf \u03ad\u03bc\u03b5\u03b9\u03bd\u03b5 \u03b1\u03c0\u03bf \u03c0\u03c5\u03c1\u03bf\u03bc\u03b1\u03c7\u03b9\u03ba\u03ac. \u0395\u03c0\u03ad\u03bb\u03b5\u03be\u03b5 \u03ac\u03bb\u03bb\u03bf \u03c3\u03b7\u03bc\u03b5\u03af\u03bf.</html>
+RNoShooterDesignated=<html>\u0394\u03b5\u03bd \u03b5\u03c7\u03b5\u03b9\u03c2 \u03bf\u03c1\u03af\u03c3\u03b5\u03b9 \u03c3\u03ba\u03bf\u03c0\u03b5\u03c5\u03c4\u03ae.\u03a0\u03ac\u03c4\u03b7\u03c3\u03b5 \u03ba\u03ac\u03c0\u03bf\u03b9\u03bf \u03c3\u03b7\u03bc\u03b5\u03af\u03bf \u03c3\u03c4\u03b7\u03bd \u03b5\u03c0\u03b9\u03c6\u03ac\u03bd\u03b5\u03b9\u03b1 <\u03b1\u03c1\u03b9\u03c3\u03c4\u03b5\u03c1\u03ac> \u03b3\u03b9\u03b1 \u03bd\u03b1 \u03c4\u03bf \u03c0\u03c1\u03ac\u03be\u03b5\u03b9\u03c2.</html>
+RNotShootable=<html>\u0394\u03b5\u03bd \u03bc\u03c0\u03bf\u03c1\u03b5\u03af\u03c2 \u03bd\u03b1 \u03c7\u03c4\u03c5\u03c0\u03ae\u03c3\u03b5\u03b9\u03c2 \u03b1\u03c5\u03c4\u03bf \u03c4\u03bf \u03c3\u03b7\u03bc\u03b5\u03af\u03bf.\u0394\u03b5\u03bd \u03b5\u03c0\u03b9\u03c4\u03c1\u03ad\u03c0\u03b5\u03c4\u03b5 \u03c4\u03bf \u03c7\u03c4\u03cd\u03c0\u03b7\u03bc\u03b1 \u03c3\u03b5 \u03b9\u03b4\u03b9 \u03c7\u03c4\u03c5\u03c0\u03b7\u03bc\u03ad\u03bd\u03bf \u03c3\u03b7\u03bc\u03b5\u03af\u03bf.</html> 
+RNotYourTurn=<html>\u03a0\u03b5\u03c1\u03af\u03bc\u03b5\u03bd\u03b5 \u03bd\u03b1 \u03ad\u03c1\u03b8\u03b5\u03b9 \u03b9 \u03c3\u03b5\u03b9\u03c1\u03ac \u03c3\u03bf\u03c5.</html>
 
 //sound
-enableSounds.label=Ήχους ενεργοποιημένος
-enableSounds.toolTipText=Ήχους ενεργοποιημένος
+enableSounds.label=\u0389\u03c7\u03bf\u03c5\u03c2 \u03b5\u03bd\u03b5\u03c1\u03b3\u03bf\u03c0\u03bf\u03b9\u03b7\u03bc\u03ad\u03bd\u03bf\u03c2
+enableSounds.toolTipText=\u0389\u03c7\u03bf\u03c5\u03c2 \u03b5\u03bd\u03b5\u03c1\u03b3\u03bf\u03c0\u03bf\u03b9\u03b7\u03bc\u03ad\u03bd\u03bf\u03c2
 
 //skins 
-skinMenu.label=Επιφάνειες
-skinMenu.toolTipText=Επιφάνειες
-skinMenuChoose.label=Επέλεξε {0}
+skinMenu.label=\u0395\u03c0\u03b9\u03c6\u03ac\u03bd\u03b5\u03b9\u03b5\u03c2
+skinMenu.toolTipText=\u0395\u03c0\u03b9\u03c6\u03ac\u03bd\u03b5\u03b9\u03b5\u03c2
+skinMenuChoose.label=\u0395\u03c0\u03ad\u03bb\u03b5\u03be\u03b5 {0}
+
+Danish.label=\u03b4\u03b1\u03bd\u03b9\u03ba\u03ae
